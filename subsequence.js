9f9fe5508bc853451bd98ce4835123c8aacae6f4
KennedyShiku <kenrichshiku@gmail.com>
2024-02-01 01:40:17 +0300

    Added subsequence and substring files

---
+++ subsequence.js
@@ -0,0 +1,4 @@
+let string = "Hello World";
+let subsequence = string.substring(1, 5);
+
+console.log(subsequence);
---
+++ substring.js
@@ -0,0 +1,4 @@
+let string = "Hello World";
+let substring = string.substring(0, 5);
+
+console.log(substring);
